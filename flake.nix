c002297e6cfd6c6946079e37443ddf2dfcad1e04
Bernd Donner <bernd.donner@sabel.com>
2024-11-11 18:48:28 +0100

    development flake added

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# run with:                nix develop
+# see metadata with:       nix flake metadata
+# debug with:              nix repl
+#                          :lf .#
+# check with:              nix flake check
+# If you want to update a locked input to the latest version, you need to ask
+# for it:                  nix flake lock --update-input nixpkgs
+# show available packages: nix-env -qa
+#                          nix search nixpkgs
+# show nixos version:      nixos-version
+# 
+
+{
+  description = "Boot.dev Environment for Nix 24.05";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "impurePythonEnv";
+          venvDir = "./.venv";
+          
+          packages = with pkgs; [
+            go
+          ];
+          
+          buildInputs = with pkgs; [
+            python3Packages.python
+            python3Packages.venvShellHook
+          ];
+
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+            pip install -r requirements.txt
+          '';
+
+        };
+      };
+    };
+}
+
